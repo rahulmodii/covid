4eaa947e5419f40f2adaead0f49161d6a07402ee
rmodi234 <rahul.modi@neologicx.com>
2020-03-28 23:37:09 +0530

    Updates

---
+++ precache-manifest.a40f70ed930db0f6025f9d989dd85115.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac626d335c198ef3ba82c8121fe96b7b",
+    "url": "/covid/index.html"
+  },
+  {
+    "revision": "9fe4a4d06b27a535ce29",
+    "url": "/covid/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ddde1a3ba9f39c76a966",
+    "url": "/covid/static/js/2.dd4ac428.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/covid/static/js/2.dd4ac428.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fe4a4d06b27a535ce29",
+    "url": "/covid/static/js/main.be538c65.chunk.js"
+  },
+  {
+    "revision": "cb89d5983b0aee4c0fd2",
+    "url": "/covid/static/js/runtime-main.f8e3727a.js"
+  }
+]);
